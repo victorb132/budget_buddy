3e5cc93d88b146984597aea9728450cbbf3374be
Victor Novais <victor96novais@gmail.com>
2023-11-05 17:58:08 -0300

    feat/bottom-navigation

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,13 +6,145 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
-        actions: [
-          Icon(Icons.search),
-          Icon(Icons.search),
-          Icon(Icons.search),
+        backgroundColor: Colors.black,
+        toolbarHeight: 100,
+        actions: const [
+          CircleAvatar(
+            radius: 40,
+            backgroundColor: Colors.white12,
+            child: Icon(
+              Icons.search,
+              color: Colors.white,
+              size: 40,
+            ),
+          ),
+          SizedBox(width: 2),
+          CircleAvatar(
+            radius: 40,
+            backgroundColor: Colors.white12,
+            child: Icon(
+              Icons.notifications_none,
+              color: Colors.white,
+              size: 30,
+            ),
+          ),
+          SizedBox(width: 2),
+          CircleAvatar(
+            radius: 40,
+            backgroundColor: Colors.white12,
+          ),
         ],
-        // leading: ,
+        leading: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                height: 34,
+                child: Image.asset('assets/images/logo.png'),
+              ),
+            ),
+            const Text(
+              'BBuddy',
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+              ),
+            )
+          ],
+        ),
+        leadingWidth: 200,
+      ),
+      bottomNavigationBar: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.all(12),
+          margin: const EdgeInsets.symmetric(horizontal: 34),
+          decoration: BoxDecoration(
+            color: Colors.white.withOpacity(0.2),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.1),
+                blurRadius: 10,
+                spreadRadius: 1,
+              ),
+            ],
+            borderRadius: const BorderRadius.all(Radius.circular(50)),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              GestureDetector(
+                onTap: () {
+                  print('Home');
+                },
+                child: Container(
+                  height: 50,
+                  width: 50,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(32)),
+                  ),
+                  child: const Icon(
+                    Icons.menu_open_outlined,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  print('Calendário');
+                },
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.transparent,
+                    borderRadius: BorderRadius.all(Radius.circular(32)),
+                  ),
+                  height: 50,
+                  width: 50,
+                  child: const Icon(
+                    Icons.calendar_month_outlined,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  print('Chat');
+                },
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.transparent,
+                    borderRadius: BorderRadius.all(Radius.circular(32)),
+                  ),
+                  height: 50,
+                  width: 50,
+                  child: const Icon(
+                    Icons.chat_bubble_outline_rounded,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  print('Configurações');
+                },
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.transparent,
+                    borderRadius: BorderRadius.all(Radius.circular(32)),
+                  ),
+                  height: 50,
+                  width: 50,
+                  child: const Icon(
+                    Icons.settings_outlined,
+                    color: Colors.white,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
